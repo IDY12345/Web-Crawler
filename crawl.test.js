b06253a6cb2650c67b7c7b886ed2f6c24bd94f37
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-09-29 14:36:02 +0530

    getURLsFromHTML Function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} =require("./crawl.js")
+const {normalizeURL,getURLsFromHTML} =require("./crawl.js")
 
 const {test,expect} =require('@jest/globals')
 
@@ -31,5 +31,80 @@ test('normalizeURL strip http',()=>
     const input='http://ishaans-portfolio.onrender.com'
     const actual=normalizeURL(input)
     const expected='ishaans-portfolio.onrender.com'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute',()=>
+{
+    const inputHTMLBody=`
+    <html>
+        <body>
+            <a href="https://ishaans-portfolio.onrender.com/">
+            Ishaan 
+            </a>
+        </body>
+    </html>        
+    `
+    const inputBaseURL="https://ishaans-portfolio.onrender.com"
+    const actual=getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected=["https://ishaans-portfolio.onrender.com/"]
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML relative',()=>
+{
+    const inputHTMLBody=`
+    <html>
+        <body>
+            <a href="/path/">
+            Ishaan 
+            </a>
+        </body>
+    </html>        
+    `
+    const inputBaseURL="https://ishaans-portfolio.onrender.com"
+    const actual=getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected=["https://ishaans-portfolio.onrender.com/path/"]
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML both',()=>
+{
+    const inputHTMLBody=`
+    <html>
+        <body>
+            <a href="https://ishaans-portfolio.onrender.com/path1/">
+            Ishaan 1
+            </a>
+            <a href="/path2/">
+            Ishaan 2
+            </a>
+        </body>
+    </html>        
+    `
+    const inputBaseURL="https://ishaans-portfolio.onrender.com"
+    const actual=getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected=["https://ishaans-portfolio.onrender.com/path1/","https://ishaans-portfolio.onrender.com/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+
+
+test('getURLsFromHTML relative',()=>
+{
+    const inputHTMLBody=`
+    <html>
+        <body>
+            <a href="Invalid">
+            Invalid URL
+            </a>
+        </body>
+    </html>        
+    `
+    const inputBaseURL="https://ishaans-portfolio.onrender.com"
+    const actual=getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected=[]
     expect(actual).toEqual(expected)
 })
